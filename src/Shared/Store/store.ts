fd6bcf23235201160007d26f02703ac2a79ff920
Ivan Horidko <yellowthunder873@gmail.com>
2025-01-06 12:52:37 +0500

    Update Shared/Store dir
    Add boilerplate code for store.ts

---
+++ src/Shared/Store/store.ts
@@ -0,0 +1,18 @@
+
+
+
+// export type AppRootState = ReturnType<typeof rootReducer>;
+
+// const rootReducer = combineReducers({
+//     todoListSlice: todoListSlice,
+//     rewardsSLice: rewardsSlice,
+//     searchTermSlice: searchTermSlice,
+// });
+
+// export const store = configureStore({
+//     reducer: rootReducer,
+// });
+
+// //Saving store to the window
+// // @ts-ignore
+// window.store = store;
