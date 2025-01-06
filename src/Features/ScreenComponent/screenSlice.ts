3dcdc7f1c3dc4758ab050a6b04b1a0a51dcd0134
Ivan Horidko <yellowthunder873@gmail.com>
2025-01-06 13:01:20 +0500

    Create screenSlice.ts
    Add boilerplate code for screenSlice

---
+++ src/Features/ScreenComponent/screenSlice.ts
@@ -0,0 +1,13 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const screenSLice = createSlice({
+	name: "screen",
+	initialState: initialState,
+	reducers: {
+
+	}
+})
+
+export default screenSLice.reducer;
