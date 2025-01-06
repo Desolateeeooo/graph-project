cb553185a5ebe860862efac1a855adae287dc588
Ivan Horidko <yellowthunder873@gmail.com>
2025-01-06 12:57:24 +0500

    Update index.tsx and store.ts
    ### store.ts
    - Add necessary imports (still with errors, but we'll fix this later)
    ### index.ts
    - Add necessary imports (still with errors)
    - Wrap in Provider

--- src/Shared/Store/store.ts
+++ src/Shared/Store/store.ts
@@ -1,18 +1,18 @@
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
+export type AppRootState = ReturnType<typeof rootReducer>;
 
+const rootReducer = combineReducers({
+    todoListSlice: todoListSlice,
+    rewardsSLice: rewardsSlice,
+    searchTermSlice: searchTermSlice,
+});
 
-// export type AppRootState = ReturnType<typeof rootReducer>;
+export const store = configureStore({
+    reducer: rootReducer,
+});
 
-// const rootReducer = combineReducers({
-//     todoListSlice: todoListSlice,
-//     rewardsSLice: rewardsSlice,
-//     searchTermSlice: searchTermSlice,
-// });
-
-// export const store = configureStore({
-//     reducer: rootReducer,
-// });
-
-// //Saving store to the window
-// // @ts-ignore
-// window.store = store;
+//Saving store to the window
+// @ts-ignore
+window.store = store;
